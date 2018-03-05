dd7bdb92412a4500d1d03d2b2ee41342d5ab374f
Harikrishnan V <39754@hexaware.com>
2018-03-05 13:08:09 +0530

    chnages in index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+const Alexa = require('alexa-sdk'),
 bodyParser = require('body-parser'),
 request = require('request'),
 config = require('./config')
@@ -16,6 +17,57 @@ app.post('/buddy',(req,res)=>{
     res.send('Welcome to chennai buddy');
 })
 
+const handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak("Goodbye!");
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak("Goodbye!");
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest': function () {
+        console.log(`Session ended: ${this.event.request.reason}`);
+    },
+    'Unhandled': function () {
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
+        this.emit(':responseReady');
+    }
+}
+
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Express server listening on port %d', server.address().port);
