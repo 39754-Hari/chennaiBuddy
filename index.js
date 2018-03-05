4ad15f3db106d34dd6602340ee9b7f1538368a97
Harikrishnan V <39754@hexaware.com>
2018-03-05 12:46:38 +0530

    changes in index

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express'),
 bodyParser = require('body-parser'),
-apiai = require('apiai'),
 request = require('request'),
 config = require('./config')
 
-
 const app = express();
 app.use(bodyParser.json());
+
+
+
+app.get('/',(req,res)=>{
+    res.send('App running');
+});
+
+
+
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Express server listening on port %d', server.address().port);
 });
+
