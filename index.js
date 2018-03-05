3259dee0a2ca37c32a6d0e2bfe1bd0efffbc65d9
Harikrishnan V <39754@hexaware.com>
2018-03-05 16:43:35 +0530

    changes in index

--- index.js
+++ index.js
@@ -15,75 +15,16 @@ app.get('/',(req,res)=>{
 
 app.post('/buddy',(req,res)=>{
     console.log('Req:',req.body.request.intent);
-    //res.send('Welcome to chennai buddy');
-    const { context, request, session } = req.body
-    const alexa = Alexa.handler(request, context);
-    alexa.APP_ID = 'amzn1.ask.skill.8f339faa-ca7f-4213-8e7b-d6c0e5b1e56f';
-    // To enable string internationalization (i18n) features, set a resources object.
-    //alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+    res.json({
+        "response": {
+            "outputSpeech": {
+              "type": "PlainText",
+              "text": "Welcome to Chennai buddy!",
+              "ssml": "<speak>Welcome to Chennai buddy!</speak>"
+            }
+        }
+    });
 })
-
-const handlers = {
-    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
-    // Alexa, ask [my-skill-invocation-name] to (do something)...
-    'LaunchRequest': function () {
-        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
-        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
-
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
-        this.emit(':responseReady');
-    },    
-    'welcomeIntent' : function() {
-        //build response first using responseBuilder and then emit
-        console.log('Inside welcomeintent');
-        this.response.speak('Hello World!');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
-        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
-        this.emit(':responseReady');
-    },
-    'AMAZON.RepeatIntent': function () {
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function () {
-        this.response.speak("Goodbye!");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak("Goodbye!");
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest': function () {
-        console.log(`Session ended: ${this.event.request.reason}`);
-    },
-    'Unhandled': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
-        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-        this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
-        this.emit(':responseReady');
-    }
-}
-
-
-exports.handler = function (event, context, callback) {
-    console.log('Inside handler');
-    const alexa = Alexa.handler(event, context, callback);
-    alexa.APP_ID = 'amzn1.ask.skill.8f339faa-ca7f-4213-8e7b-d6c0e5b1e56f';
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
-
 
 buddy.launch(function(request, response) {
     console.log('Inside launch');
