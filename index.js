0b82df15b476b6feaceef374a6974f82b49352fc
Harikrishnan V <39754@hexaware.com>
2018-03-06 12:47:14 +0530

    changes in files

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/',(req,res)=>{
 app.post('/buddy',(req,res)=>{
     console.log('Req:',req.body.request.intent);
     let intent = require('./'+req.body.request.intent.name);
-    intent.requestHandler(req,res)
+    intent.requestHandler(req,res);
 })
 
 buddy.launch(function(request, response) {
