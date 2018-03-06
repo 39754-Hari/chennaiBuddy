dd3061778aa7fac5de946efe6f47e04ec99f8f95
Harikrishnan V <39754@hexaware.com>
2018-03-06 15:34:09 +0530

    changes for weather api

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 var config ={
-    'fuelApiUrl' : 'https://newsrain-petrol-diesel-prices-india-v1.p.mashape.com/capitals'
+    'fuelApiUrl' : 'https://newsrain-petrol-diesel-prices-india-v1.p.mashape.com/capitals',
+    'weatherForecastUrl' : 'http://api.apixu.com/v1/forecast.json?key=ac9e8a54185f411e81d74031180603&q=',
+    'weatherHistoryUrl' : 'http://api.apixu.com/v1/history.json?key=ac9e8a54185f411e81d74031180603&q=',
+    'currentWeatherUrl' : 'http://api.apixu.com/v1/current.json?key=ac9e8a54185f411e81d74031180603&q='
 };
 
 module.exports = config;
---
+++ weatherIntent.js
@@ -0,0 +1,58 @@
+const request = require('request'),
+config = require ('./config');
+
+var functions={
+    requestHandler : function(req,res) {
+        console.log();
+        var city = req.body.request.intent.slots.city.value;
+        var userDate = req.body.request.intent.slots.date.value;
+        var type = req.body.request.intent.slots.type.value;
+        var currentDate = new Date().toISOString().slice(0,10);
+        var enteredDate = new Date(userDate).toISOString().slice(0,10);
+        if(city != null && typeof city != "undefiend"){
+            if(userDate != null && typeof userDate != "undefined"){
+                if (currentDate < enteredDate){
+                    weatherForecast(city,userDate);
+                }
+                else if(currentDate > enteredDate){
+                    weatherHistory(city,userDate)
+                }
+                else{
+                    getWeather(city);
+                }
+            }
+            else{
+                getWeather(city);
+            }
+        }
+        else{
+            cityMissed();
+        }
+    }
+};
+
+function weatherForecast(city,date){
+    var forecastDate = new Date(date);
+    var difference = new Date( forecastDate -new Date());
+    var days = difference.getDate();
+    let options ={
+        url : config.weatherForecastUrl+city+'&days='+days,
+        headers : {  "Accept": "application/json"},
+        method : 'GET',            
+      json: true
+    }
+    request(options, (error, response) => {
+        if (!error && response.statusCode == 200) {
+            console.log(response);
+            
+        } else if (response.statusCode == 404) {
+            console.log('Inside 404');
+            console.log('');
+        } else {
+            console.log('Inside error');
+            console.log(error);
+        }
+    });
+}
+
+module.exports = functions;
