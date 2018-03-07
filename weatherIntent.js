3f5c6deae36f82aa41024a7e070c6a37026eb0b6
Harikrishnan V <39754@hexaware.com>
2018-03-07 10:59:11 +0530

    changes for weather api

--- weatherIntent.js
+++ weatherIntent.js
@@ -52,7 +52,7 @@ function weatherForecast(city,date,res){
                     {
                         console.log(forecastday[index].day);
                         var responseText = 'The weather in '+ city + ' on '+ date + ' forecasted as, '+forecastday[index].day.condition.text +
-                        '. maximum temperature can be upto'+ forecastday[index].day.maxtemp_c +' degree Celsius , minimum temperature will be '+ forecastday[index].day.mintemp_c + 
+                        '. maximum temperature can be upto,'+ forecastday[index].day.maxtemp_c +' degree Celsius , minimum temperature will be '+ forecastday[index].day.mintemp_c + 
                         ' degree Celsius.';
                     var responseJson = {
                         "response": {
