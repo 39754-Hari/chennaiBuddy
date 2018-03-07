bf8161b0a9268a9a46f4af417fa386341bcbcd56
Harikrishnan V <39754@hexaware.com>
2018-03-07 12:11:22 +0530

    Changes for train intent

--- trainIntent.js
+++ trainIntent.js
@@ -4,8 +4,8 @@ var functions={
     requestHandler : function(req,res) {
         var sourceCity = req.body.request.intent.slots.source.value;
         var destinationCity = req.body.request.intent.slots.destination.value;
-        var responseText = 'Trais available from '+sourceCity + ', to '+destinationCity +
-        'are, , Chennai express departing on 06:50 hours, MUMBAI EXPRESS departing on 12:20 hours, MUMBAI MAIL departing on 23:55 hours';
+        var responseText = 'Trains available from '+sourceCity + ' to '+destinationCity +
+        ' are, , Chennai express departing on 06:50 hours, MUMBAI EXPRESS departing on 12:20 hours, MUMBAI MAIL departing on 23:55 hours';
         var responseJson = {
             "response": {
                 "outputSpeech": {
