b8a7d20e56c31b050bc0e8d7c0d97c1b5959f88b
Harikrishnan V <39754@hexaware.com>
2018-03-07 10:08:42 +0530

    changes for weather api

--- weatherIntent.js
+++ weatherIntent.js
@@ -16,7 +16,7 @@ var functions={
                     
                 }
                 else if(currentDate > enteredDate){
-                    weatherHistory(city,userDate)
+                    weatherHistory(city,userDate,res)
                 }
                 else{
                     getWeather(city),res;
@@ -101,6 +101,47 @@ function getWeather(city,res){
                     res.json (responseJson).end();   
                     return 'success' ;
                     
+        } else if (response.statusCode == 404) {
+            console.log('Inside 404');
+            console.log('');
+        } else {
+            console.log('Inside error');
+            console.log(error);
+        }
+    });
+}
+
+function weatherHistory(city,date,res){
+    let options ={
+        url : config.weatherHistoryUrl+city+'&dt='+date,
+        headers : {  "Accept": "application/json"},
+        method : 'GET',            
+      json: true
+    }
+    request(options, (error, response) => {
+        if (!error && response.statusCode == 200) {
+            console.log(response.body.forecast);
+            var forecastday = response.body.forecast.forecastday;
+            for (var index = 0; index < forecastday.length; ++index) {
+                if(forecastday[index].date === date)
+                    {
+                        console.log(forecastday[index].day);
+                        var responseText = city + '\'s weather on '+ date + ' was , '+forecastday[index].day.condition.text +
+                        '. maximum temperature recorded as '+ forecastday[index].day.maxtemp_c +' degree Celsius , and minimum temperature was '+ forecastday[index].day.mintemp_c + 
+                        ' degree Celsius.';
+                    var responseJson = {
+                        "response": {
+                            "outputSpeech": {
+                              "type": "PlainText",
+                              "text": responseText,
+                              "ssml": "<speak>"+responseText+"</speak>"
+                            }
+                        }
+                    }
+                    res.json (responseJson).end();   
+                    return 'success' ;
+                    }
+            }
         } else if (response.statusCode == 404) {
             console.log('Inside 404');
             console.log('');
